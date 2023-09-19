c41a53c84b8a2d0a883f3f958d7dff0014bfe107
romulo.banares <romulo88@hotmail.com>
2023-09-19 18:45:31 +0200

    add initial commit

---
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.example.mymovies
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mymovies.ui.theme.MyMoviesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyMoviesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ButtonText()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+@Preview(
+    showBackground = true
+)
+@Composable
+fun MediaItem() {
+    Column {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+                .background(Color.Red)
+        ) {}
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Title 1")
+        }
+    }
+}
+
+/*@Preview(
+    widthDp = 200,
+    heightDp = 100,
+    showBackground = true
+)*/
+@Composable
+fun ButtonText() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello World",
+            modifier = Modifier
+                .clickable { /* TODO */ }
+                .background(Color.Cyan)
+                .border(width = 2.dp, color = Color.Blue)
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+    }
+}
+
+/*@Preview(
+    showBackground = true,
+    name = "Android greeting",
+    widthDp = 400,
+    heightDp = 200
+)*/
+@Composable
+fun GreetingPreview() {
+    MyMoviesTheme {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Greeting(
+                "Compose",
+                modifier = Modifier
+                    .background(Color.LightGray)
+                    .weight(weight = 2f)
+            )
+            Greeting(
+                "Compose",
+                modifier = Modifier
+                    .background(Color.Yellow)
+                    .weight(weight = 1f)
+            )
+        }
+    }
+}
